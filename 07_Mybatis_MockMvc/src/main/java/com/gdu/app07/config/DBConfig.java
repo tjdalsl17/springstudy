3afbdc159b8cc10e9b5a782980f6f7c1d5551dfc
tjdalsl17 <1117ysm@naver.com>
2023-04-20 19:01:29 +0900

    2023-04-20

---
+++ 07_Mybatis_MockMvc/src/main/java/com/gdu/app07/config/DBConfig.java
@@ -0,0 +1,63 @@
+package com.gdu.app07.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@PropertySource(value={"classpath:application.properties"})	// application.properties 파일의 속성을 읽어 오자!
+@EnableTransactionManagement 	// 트랜잭션 처리를 허용한다.
+@Configuration
+public class DBConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	// HikariConfig Bean
+	@Bean
+	public HikariConfig hikariConfig() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName(env.getProperty("spring.datasource.hikari.driver-class-name"));
+		hikariConfig.setJdbcUrl(env.getProperty("spring.datasource.hikari.jdbc-url"));
+		hikariConfig.setUsername(env.getProperty("spring.datasource.hikari.username"));
+		hikariConfig.setPassword(env.getProperty("spring.datasource.hikari.password"));
+		return hikariConfig;
+	}
+	// HikariDataSource Bean
+	@Bean(destroyMethod="close")
+	public HikariDataSource datasource() {
+		return new HikariDataSource(hikariConfig());
+	}
+	
+	// SqlSessionFactory Bean
+	@Bean
+	public SqlSessionFactory factory() throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(datasource());
+		bean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource(env.getProperty("mybatis.config-location")));
+		bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(env.getProperty("mybatis.mapper-locations")));
+		return bean.getObject();
+	}
+	// SqlSessionTemplate Bean (기존의 SqlSession)
+	@Bean
+	public SqlSessionTemplate template() throws Exception {
+		return new SqlSessionTemplate(factory());
+	}
+	
+	// TransactionManager Bean
+	@Bean
+	public TransactionManager transactionManager() {
+		return new DataSourceTransactionManager(datasource());
+	}
+}
